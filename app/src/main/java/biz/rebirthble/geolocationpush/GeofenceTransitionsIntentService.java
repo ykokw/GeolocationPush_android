ca6418c472b3cf51d21c793e7d4f78aa334d0e2d
Yuki Okawa <okawa.yuki@nifty.co.jp>
2015-10-22 16:29:31 +0900

    fixed to open MainActivity when notification is opened

--- app/src/main/java/biz/rebirthble/geolocationpush/GeofenceTransitionsIntentService.java
+++ app/src/main/java/biz/rebirthble/geolocationpush/GeofenceTransitionsIntentService.java
@@ -2,8 +2,10 @@ package biz.rebirthble.geolocationpush;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
@@ -60,6 +62,22 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
 
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
