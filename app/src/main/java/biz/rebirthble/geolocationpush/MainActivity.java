35951a7e61b25f0b5f3f553c41d92f6259862ab6
Yuki Okawa <okawa.yuki@nifty.co.jp>
2015-12-16 10:55:51 +0900

    Runtime Permission Requestの対応

--- app/src/main/java/biz/rebirthble/geolocationpush/MainActivity.java
+++ app/src/main/java/biz/rebirthble/geolocationpush/MainActivity.java
@@ -2,7 +2,10 @@ package biz.rebirthble.geolocationpush;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +17,8 @@ import com.nifty.cloud.mb.core.NCMBException;
 import com.nifty.cloud.mb.core.NCMBInstallation;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +55,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Android 6.0向けのアプリ実行時パーミッション許可画面表示
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+    //パーミッション許可画面からのハンドリング
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults)
+    {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "ACCESS_FINE_LOCATION is granted!");
+                } else {
+                    Log.d("MainActivity", "ACCESS_FINE_LOCATION is denied!");
+                }
+                return;
+            }
+        }
     }
 
     @Override
